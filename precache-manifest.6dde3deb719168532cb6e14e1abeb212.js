8a739a0277eca7b451759cd7b5bc428654caf5a5
manan2501 <kevadiyamanan@gmail.com>
2019-12-26 04:36:46 +0530

    Updates

---
+++ precache-manifest.6dde3deb719168532cb6e14e1abeb212.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a496baeaa655a3e5c763968e056ec8be",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "6c8cd98a35ac92f6488f",
+    "url": "/todo-list/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "84a5c18a8ae966226f8d",
+    "url": "/todo-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6c8cd98a35ac92f6488f",
+    "url": "/todo-list/static/js/2.74eeaad0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-list/static/js/2.74eeaad0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84a5c18a8ae966226f8d",
+    "url": "/todo-list/static/js/main.fc7c470a.chunk.js"
+  },
+  {
+    "revision": "346cf83612625f5a0ae4",
+    "url": "/todo-list/static/js/runtime-main.5c2057b1.js"
+  }
+]);
